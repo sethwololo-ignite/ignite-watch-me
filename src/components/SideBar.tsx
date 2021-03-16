63383cd0d1bbf0bc8ffacf8af24db87d989fec35
sethwololo <sethwololo@gmail.com>
2021-03-16 11:51:48 -0300

    Aplicação componentizada

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,37 @@
-export function SideBar() {
-  // Complete aqui
+
+import '../styles/sidebar.scss';
+
+import { Button } from './Button';
+
+interface GenreProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SideBarProps {
+  genres: GenreProps[];
+  selectedGenreId: number;
+  buttonClick: (id: number) => void;
+}
+
+export function SideBar({genres, selectedGenreId, buttonClick}: SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            id={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => buttonClick(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  );
 }
